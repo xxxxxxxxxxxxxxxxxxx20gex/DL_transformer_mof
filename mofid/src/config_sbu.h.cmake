35f55586ba1ff90fd4e441fc0f326ba86e248653
ZilinGao <gzl@mail.dlut.edu.cn>
2025-10-29 20:52:31 +0800

    del: mofid

--- mofid/src/config_sbu.h.cmake
+++
@@ -1,6 +0,0 @@
-/* Based on openbabel/src/config.h.cmake */
-
-/* Where the data files are located */
-#define LOCAL_OB_DATADIR "@LOCAL_OB_DATADIR@"
-/* Where the shared libraries are located */
-#define LOCAL_OB_LIBDIR "@LOCAL_OB_LIBDIR@"
